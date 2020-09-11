03af806fe94d1e4e7dd060097e272fe3f67aa76a
rishikesh312 <54978564+rishikesh312@users.noreply.github.com>
2020-09-11 14:50:43 +0530

    Update predict.py

--- Prediction/predict.py
+++ Prediction/predict.py
@@ -1,38 +1,27 @@
-#add your imports here
+
 from __future__ import division
 from __future__ import print_function
-# own imports
 import ntpath
 
-#custom classes imports
+
 import kerasmodel as km
 import segmentor2 as seg
 
-#Separate ImgPred and SymPred to files
+
 from SymPred import SymPred
 from ImgPred import ImgPred
 
-#original imports
+
 from sys import argv
 from glob import glob
 from scipy import misc
 import numpy as np
 import random
 
-"""
-add whatever you think it's essential here
-"""
-#load keras model and label encoder
 model, le = km.KerasModel.load()
 
 def predict(image_path):
-	"""
-	Add your code here
-	"""
-	"""
-	#Don't forget to store your prediction into ImgPred
-	img_prediction = ImgPred(...)
-	"""
+
 	sym_preds = []
 	print("Predicting "+ ntpath.basename(image_path))
 	imgs, rects = seg.process(image_path)
